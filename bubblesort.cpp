c7afa45d1262bfe4ac0bbbf3b649a84b155434dd
BagasGandhi <saputramuhammadgandhi@gmail.com>
2025-03-13 11:56:04 +0700

    step 3

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -1,6 +1,10 @@
-do
-    {
-       for (int j = 0; j <= n -1-pass;j++)
-        {  //step 2
+if (arr[j] > arr[j+1]) // step 3
+            {
+                int temp;
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
 
-            
+     
