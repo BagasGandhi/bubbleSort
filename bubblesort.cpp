2f8f7e55e70fa420f315c55fe2be5e62b5cbba41
BagasGandhi <saputramuhammadgandhi@gmail.com>
2025-03-13 11:51:50 +0700

    Deklarasi variable dan nProsedur input

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -1,7 +1,3 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
 int arr[20];     // Deklarasi variable global array a dengan ukuran 20
 int n;           // Deklarasi variable global n untuk menyimpan banyaknya elemen pada array
 
@@ -24,56 +20,4 @@ void input() {   // procedur untuk input
         cout << "Data ke-" << (i+1) << ";";
         cin >> arr{i};
     }
-}
-
-void bubleSortArray(){
-    int pass = 1;  //step 1
-
-    do
-    {
-       for (int j = 0; j <= n -1-pass;j++)
-        {  //step 2
-
-            if (arr[j] > arr[j+1])
-            {
-                int temp;
-                temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-            }
-        }
-
-     pass = pass + 1; // step 4
-        
-    } while (pass <= n - 1); // step 5
-}
-
-void display() {
-    cout << endl;
-    cout << "===================" << endl;
-    cout << "Element Array yang telah tersusun" << endl;
-    cout << "================" << endl;
-    cout << endl;
-    for (int j = 0; j < n; j++) {
-        cout << arr[j]; // menampilkan array
-        if (j < n - 1) {
-            cout << " --> ";
-        }
-    }
-    cout << endl;
-    cout << endl;
-
-    cout << "Jumlah pass = " << n - 1 << endl; // menampilkan jumlah dari pass
-    cout << endl;
-    cout << endl;
-}
-
-int main() {
-    input();
-
-    bubbleSortArray();
-    display();
-
-    system("pause");
-    return 0;
 }
